0b5fb7a59da00047a4f60ce51e133b55c3b08e7b
palascinochristian <palascinochristian@gmail.com>
2024-12-05 15:53:09 +0100

    second exercise done

--- ex-js-functions-snack-main/02 - Filtra iniziali/script.js
+++ ex-js-functions-snack-main/02 - Filtra iniziali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const userLetter = "A"
 
 
 // Dichiara la funzione qui.
-
+const filterInitials = (array, letter) => {
+    const filteredArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] === letter) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(filterInitials(names, userLetter))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
