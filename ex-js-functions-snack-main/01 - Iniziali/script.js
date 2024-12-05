6b4b27d5737746fb1cb89d75a9f4a2a54ef123f9
palascinochristian <palascinochristian@gmail.com>
2024-12-05 15:39:13 +0100

    first exercise done

--- ex-js-functions-snack-main/01 - Iniziali/script.js
+++ ex-js-functions-snack-main/01 - Iniziali/script.js
@@ -2,11 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+
 // Dichiara la funzione qui.
 
+const getInitials = (array) => {
+    const arrayInitials = [];
+for(let i = 0; i < array.length; i++){
+    arrayInitials.push(array[i][0]);
+}
+return arrayInitials;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
