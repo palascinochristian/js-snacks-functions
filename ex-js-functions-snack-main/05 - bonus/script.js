cd2047c45dfbd675cd0df772905d27b7bf176167
palascinochristian <palascinochristian@gmail.com>
2024-12-05 17:53:31 +0100

    bonus exercise done

--- ex-js-functions-snack-main/05 - bonus/script.js
+++ ex-js-functions-snack-main/05 - bonus/script.js
@@ -9,10 +9,25 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+const greetOnTime = (name) => {
+    const time = new Date().getHours();
+    
+    if (time >= 5 && time < 13) {
+        return `Buongiorno ${name}! Sono le ${time}!`;
+    } else if (time >= 14 && time <= 17) {
+        return `Buon pomeriggio ${name}! Sono le ${time}!`;
+    } else if (time >= 18 && time <= 24) {
+        return `Buonasera ${name}! Sono le ${time}!`;;
+    } else {
+        return `Sono le ${time} di notte! Vai a letto!! Che ci fai sveglio ${name}!?`;
+    }
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(greetOnTime(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
