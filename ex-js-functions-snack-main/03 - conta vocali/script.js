747928f8de4efa159bd676abb657a0957031229c
palascinochristian <palascinochristian@gmail.com>
2024-12-05 16:15:47 +0100

    third exercise done

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const getVowelsNum = (word) => {
+    const vowels = [];
+for(let i = 0; i < word.length; i++){
+    if(word[i] === "a" ||
+       word[i] === "e" || 
+       word[i] === "i" || 
+       word[i] === "o" || 
+       word[i] === "u" ){
+        vowels.push(word[i])
+    }
+}
+return vowels.length;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getVowelsNum(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
